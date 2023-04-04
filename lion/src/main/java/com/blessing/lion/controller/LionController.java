6e16be3b76fc95ce87422313c7d072ec78334265
mothusimolorane <molorane91@gmail.com>
2023-04-04 09:08:24 +0200

    changes

--- lion/src/main/java/com/blessing/lion/controller/LionController.java
+++ lion/src/main/java/com/blessing/lion/controller/LionController.java
@@ -1,16 +1,20 @@
 package com.blessing.lion.controller;
 
 import com.blessing.lion.config.LionConfig;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("lion")
+@AllArgsConstructor
 public class LionController {
 
+    final LionConfig lionConfig;
+
     @GetMapping("vendor")
-    public String getVendor(final LionConfig lionConfig) {
+    public String getVendor() {
         return lionConfig.getVendor();
     }
 }
--- rhino/src/main/java/com/blessing/rhino/controller/RhinoController.java
+++ rhino/src/main/java/com/blessing/rhino/controller/RhinoController.java
@@ -1,16 +1,20 @@
 package com.blessing.rhino.controller;
 
 import com.blessing.rhino.config.RhinoConfig;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("lion")
+@RequestMapping("rhino")
+@AllArgsConstructor
 public class RhinoController {
 
+    final RhinoConfig rhinoConfig;
+
     @GetMapping("vendor")
-    public String getVendor(final RhinoConfig rhinoConfig) {
+    public String getVendor() {
         return rhinoConfig.getVendor();
     }
 }
