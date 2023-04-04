c6199e335d9706f1bb1a4c5dc308ba82955af23e
mothusimolorane <molorane91@gmail.com>
2023-04-04 13:50:29 +0200

    configs

--- lion/src/main/java/com/blessing/lion/config/LionConfig.java
+++ lion/src/main/java/com/blessing/lion/config/LionConfig.java
@@ -1,17 +1,10 @@
 package com.blessing.lion.config;
 
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
-import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
-import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-
-import java.time.Duration;
 
 @Configuration
 public class LionConfig {
@@ -22,14 +15,4 @@ public class LionConfig {
         return new RestTemplateBuilder()
                 .build();
     }
-
-    @Bean
-    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
-        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
-                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build())
-                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
-                .build());
-    }
-
-
 }
